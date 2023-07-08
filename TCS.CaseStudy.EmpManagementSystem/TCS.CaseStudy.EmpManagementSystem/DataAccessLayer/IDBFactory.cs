f569da445419af08f574e4569ce2df68ff8336dc
TijuThampan <129253367+TijuThampan@users.noreply.github.com>
2023-07-09 00:01:04 +0530

    Add files via upload

---
+++ TCS.CaseStudy.EmpManagementSystem/TCS.CaseStudy.EmpManagementSystem/DataAccessLayer/IDBFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCS.CaseStudy.EmpManagementSystem.DataAccessLayer
+{
+    public interface IDBFactory<T> where T : class
+    {
+        Hashtable Parameter { get; set; }
+        Dictionary<string, SqlDbType> OutParam { get; set; }
+        List<T> GetData(string sql, CommandType type);
+        bool AddOrUpdOrDelData(string sql, CommandType type);
+    }
+}
